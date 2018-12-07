7b35c89b2d710c245af2581ed25d0be165c60942
Daniel Kaiser <dankaiser1808@gmail.com>
2018-12-07 16:26:40 +0100

    testmap html

---
+++ leafletmap.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+    var basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	});
+
+var opcIcon = L.icon({
+    iconUrl: 'opc.png',
+
+    iconSize:     [30, 30],
+    iconAnchor:   [22, 94],
+    popupAnchor:  [-3, -76]
+});
+
+    $.getJSON("census.geojson", function(data) {
+
+    var geojson = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.Area_Name);
+      }
+    });
+
+
+    var map = L.map('my-map')
+    .fitBounds(geojson.getBounds());
+
+
+    basemap.addTo(map);
+    geojson.addTo(map);
+    });
+
+};
