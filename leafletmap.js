4d21715e338f9132f9bd7c604bbe60fee8f05e73
Daniel Kaiser <dankaiser1808@gmail.com>
2018-12-18 23:19:34 +0100

    changed minZoom

--- leafletmap.js
+++ leafletmap.js
@@ -2,7 +2,7 @@ window.onload = function () {
     console.log("Script is running")
     var basemap = L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-        noWrap: true
+        noWrap: false
 	});
 
     $.getJSON("census.geojson", function(data) {
@@ -13,12 +13,12 @@ window.onload = function () {
       }
     });
 
-    var map = L.map('my-map')
+    var map = L.map('my-map',{
+      minZoom: 2
+    })
     .fitBounds(geojson.getBounds());
 
     basemap.addTo(map);
     geojson.addTo(map);
-$(window).on("resize", function () {
-     $("#my-map").height($(window).height()); map.invalidateSize(); }).trigger("resize");
     });
 };
