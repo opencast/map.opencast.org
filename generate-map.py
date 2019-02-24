3a80ad3ab6db2ad17b5c0bab13ace23f6e2956df
Daniel Kaiser <dankaiser1808@gmail.com>
2019-02-24 21:22:26 +0100

    created a compareCache method to reduce the number of geolocation requests

--- generate-map.py
+++ generate-map.py
@@ -3,36 +3,82 @@
 import sqlite3
 import json
 from geopy.geocoders import Nominatim
+import os
 
 
 def getUserInformation():
-    geolocator = Nominatim(timeout=10, user_agent='Opencast map generator')
+
+    locations = []
 
     cur = sqlite3.connect('user.db').cursor()
     cur.execute('select distinct country, city, organization from user')
     for country, city, organization in cur.fetchall():
-        location = geolocator.geocode({'country': country, 'city': city},
-                                      addressdetails=True) \
-                   or geolocator.geocode('%s, %s' % (country, city),
-                                         addressdetails=True)
-        print((country, city), '->', location)
-        if location:
-            yield (location.latitude, location.longitude, organization)
+        if organization != "None":
+            locations.append(compareCache(country, city, organization))
+    return locations
+
+
+def getGeoCode(country, city, organization, dataList):
+
+    geolocator = Nominatim(timeout=10, user_agent='Opencast map generator')
+    newLocation = geolocator.geocode(country, city, addressdetails=True) \
+        or geolocator.geocode('%s, %s' % (country, city), addressdetails=True)
+    if newLocation:
+        os.remove("cache.json")
+        f = open("cache.json", "a")
+        geoLocation = {"country": country, "city": city,
+                       "organization": organization,
+                       "latitude": newLocation.latitude,
+                       "longitude": newLocation.longitude}
+        dataList.append(geoLocation)
+        f.write(json.dumps(dataList))
+
+        geoLocation = {"country": country,
+                       "city": city, "organization": organization,
+                       "latitude": newLocation.latitude,
+                       "longitude": newLocation.longitude}
+        return geoLocation
 
 
 def convertGeoJson(addresses):
+
     features = []
-    for lat, lon, org in addresses:
-        features.append({
-            "type": "Feature",
-            "properties": {
-                'institution': org,
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [lon, lat]
-            }})
+    for i in addresses:
+        if i is not None:
+            print(i)
+            features.append({
+                "type": "Feature",
+                "properties": {
+                    'institution': i["organization"]
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [i["longitude"], i["latitude"]]
+                }})
     return {"type": "FeatureCollection", "features": features}
+
+
+def compareCache(country, city, organization):
+
+    check = 0
+    with open('cache.json') as data_file:
+        if os.stat("cache.json").st_size == 0:
+            data = []
+
+            return getGeoCode(country, city, organization, data)
+        else:
+            data = json.load(data_file)
+            for item in data:
+                if (city == item["city"] and country == item['country']
+                        and organization == item['organization']):
+                    check = 1
+                    if check == 1:
+
+                        return item
+            else:
+
+                return getGeoCode(country, city, organization, data)
+            check = 0
 
 
 def main():
