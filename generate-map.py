7491bb8537318fe327615dcb9f9cd3efd5133f51
Daniel Kaiser <dankaiser1808@gmail.com>
2019-05-06 19:52:50 +0200

    multiple institutions added on single marker, refactored code logic, set leafletmap back to master Version with 4 spaces instead of 2

--- generate-map.py
+++ generate-map.py
@@ -2,28 +2,28 @@
 
 import sqlite3
 import json
+import itertools
+from operator import itemgetter
 from geopy.geocoders import Nominatim
 
-
-# global geolocator for initializing only ones
-def set_globalGeolocator():
-
-    global geolocator
-    geolocator = Nominatim(timeout=10, user_agent='Opencast map generator')
+global geolocator
+geolocator = Nominatim(timeout=10, user_agent='Opencast map generator')
 
 
 # load the file only ones
 def load_datafile():
 
     global data
-    with open("cache.json", "w+") as data_file:
-        try:
+
+    try:
+        with open("cache.json", "r") as data_file:
             data = data_file.read()
             data = json.loads(data)
-        except Exception:
+    except Exception:
+        with open("cache.json", "w+") as data_file:
             # Because of the empty file python converts data to a String, so
             # in line 46 append is not working.
-            #  --> In Exception data = [] to prefend this problem.
+            #  --> In Exception ,,data = []'' to prevent this problem.
             print("FILE IS EMPTY")
             data = []
 
@@ -39,10 +39,10 @@ def getUserInformation():
 
 def getGeoCode(country, city, organization, dataList):
 
-    # Location with geocode not None ->
-    # add it to the cache and return the location
-    newLocation = geolocator.geocode(country, city, addressdetails=True) \
-        or geolocator.geocode('%s, %s' % (country, city), addressdetails=True)
+    # Location with geocode true ->
+    # add it to the cache and return geoLocation object
+    newLocation = geolocator.geocode('%s, %s' %
+                                     (country, city), addressdetails=True)
     if newLocation:
         geoLocation = {"country": country, "city": city,
                        "organization": organization,
@@ -51,7 +51,44 @@ def getGeoCode(country, city, organization, dataList):
         dataList.append(geoLocation)
         with open("cache.json", "w") as f:
             f.write(json.dumps(dataList))
+        print(geoLocation)
         return geoLocation
+
+
+def fixOverlappingEntries():
+
+    sorted_all = sorted(data, key=itemgetter('longitude'))
+    grouped_locations = []
+
+    # group all Organizations by long and latitude and save them in a list
+    for key, group in (
+                       itertools.groupby(sorted_all,
+                                         key=lambda each: (each['longitude'],
+                                                           each["latitude"]))
+    ):
+        grouped_locations.append(list(group))
+
+    pooledLoc = []
+    # create Location with multiple organizations
+    try:
+        for k in range(len(grouped_locations)):
+            pooledLocation = {"country": grouped_locations[k][0]["country"],
+                              "city": grouped_locations[k][0]["city"],
+                              "organization": "",
+                              "latitude": grouped_locations[k][0]["latitude"],
+                              "longitude": grouped_locations[k][0]["longitude"]
+                              }
+            for grp_location in grouped_locations[k]:
+                pooledLocation["organization"] += grp_location["organization"]
+                pooledLocation["organization"] += ", "
+                pooledLoc.append(pooledLocation)
+
+        for l in range(len(pooledLoc)):
+            pooledLoc[l]["organization"] = pooledLoc[l]["organization"][:-2]
+
+        return pooledLoc
+    except TypeError:
+        return None
 
 
 def convertGeoJson(addresses):
@@ -78,10 +115,11 @@ def compareCache(country, city, organization):
         for item in data:
             if (city == item["city"] and country == item['country']
                     and organization == item['organization']):
-                # print("USER LOADED FROM CACHE")
+                print("USER LOADED FROM CACHE")
+                print(item)
                 return item
         else:
-            # print("NEW USER NOT IN CACHE")
+            print("NEW USER NOT IN CACHE")
             return getGeoCode(country, city, organization, data)
     except Exception:
         print("FAILED ERROR --> Empty, Not Existing File etc.")
@@ -89,10 +127,10 @@ def compareCache(country, city, organization):
 
 
 def main():
-    set_globalGeolocator()
     load_datafile()
+    convertGeoJson(getUserInformation())
     with open("adopters.geojson", "w") as census:
-        census.write(json.dumps(convertGeoJson(getUserInformation())))
+        census.write(json.dumps(convertGeoJson(fixOverlappingEntries())))
 
 
 if __name__ == '__main__':
--- leafletmap.js
+++ leafletmap.js
@@ -6,28 +6,20 @@ window.onload = function () {
     });
 
     $.getJSON("adopters.geojson", function (data) {
-         function onEachFeature(features, layer) {
 
-            marker = " ";
+        function onEachFeature(feature, layer) {
 
-            data.features.every(position => {
-                if (features.geometry.coordinates[1] == position.geometry.coordinates[1] && features.geometry.coordinates[0] == position.geometry.coordinates[0]){
-                   layer.bindPopup("Institutions: " + features.properties.institution + ", " + position.properties.institution);
-                } else {
-                    layer.bindPopup("Institutions: " + features.properties.institution);
-                } 
-            });
+            layer.bindPopup("Institution: " + feature.properties.institution);
         }
-         var geojson = L.geoJson(data, {
+        var geojson = L.geoJson(data, {
+            onEachFeature: onEachFeature
+        });
 
-             onEachFeature: onEachFeature
-         });
-
-         var map = L.map('my-map', {
-
+        var map = L.map('my-map', {
             minZoom: 2
-        }).fitBounds(geojson.getBounds());
-  
+        })
+            .fitBounds(geojson.getBounds());
+
         basemap.addTo(map);
         geojson.addTo(map);
     });
